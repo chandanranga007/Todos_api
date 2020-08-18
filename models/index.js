08eca5ea2451960d5511277e463ce44784919cf7
Chandan Ranga <chandanranga007@gmail.com>
2020-08-18 11:47:38 +0530

    first commit

---
+++ models/index.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+mongoose.set('debug',true);
+
+     mongoose.connect("mongodb://localhost/todo",{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+     }); 
+
+mongoose.Promise = Promise;      
+
+module.exports.Todo = require('./todo');
